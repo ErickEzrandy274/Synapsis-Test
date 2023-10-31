bfaf786aa50e814282c577733ed9d382cad010e2
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-10-31 14:26:56 +0700

    refactor: conditional rendering when to use layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "@/components/modules/Layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const { pathname } = useRouter();
+
 	return (
 		<ChakraProvider>
-			<Component {...pageProps} />
+			{pathname === "/" ? (
+				<Component {...pageProps} />
+			) : (
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			)}
 		</ChakraProvider>
 	);
 }
