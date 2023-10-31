b256fd665e9ad525b03a72f33042baff7a0baee4
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-10-31 20:30:22 +0700

    feat: finish implement blog details page

---
+++ components/modules/Blog/BlogDetails.tsx
@@ -0,0 +1,99 @@
+import { Badge, Box, Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { BlogDetailProps } from "./interface";
+import { useRouter } from "next/router";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+
+const BlogDetails: React.FC<BlogDetailProps> = ({
+	isLoading,
+	commentsData,
+	userData,
+	postDetails,
+}) => {
+	const router = useRouter();
+	const handleBack = () => {
+		router.back();
+	};
+
+	return (
+		<Flex flexDir="column" gap={3}>
+			<Flex cursor="pointer" alignItems="center" w="fit-content">
+				<ArrowBackIcon />
+				<Text fontSize="md" fontWeight="semibold" onClick={handleBack}>
+					Back
+				</Text>
+			</Flex>
+			<Flex
+				justifyContent="space-between"
+				gap={5}
+				flexDir={{ base: "column", md: "row" }}
+			>
+				<Flex w={{ base: "full", md: "50%" }}>
+					<Flex flexDir="column" gap={3}>
+						<Heading as="h5" size="md">
+							Description
+						</Heading>
+						<Text>{postDetails?.body ?? "No description!"}</Text>
+						<Flex flexDir="column" gap={2}>
+							<Heading as="h5" size="md">
+								Made By
+							</Heading>
+							{userData?.name ? (
+								<>
+									<Box rounded="md" bg="gray.200" w="fit-content" p={2}>
+										<Text as="h5" size="sm">
+											Name: {userData.name}
+										</Text>
+										<Text as="h5" size="sm">
+											Email: {userData.email}
+										</Text>
+										<Text as="h5" size="sm">
+											Gender: {userData.gender}
+										</Text>
+										<Text as="h5" size="sm">
+											Status:{" "}
+											<Badge colorScheme="messenger">{userData.status}</Badge>
+										</Text>
+									</Box>
+								</>
+							) : (
+								<Text as="h5" size="md">
+									Unknown
+								</Text>
+							)}
+						</Flex>
+					</Flex>
+				</Flex>
+
+				<Flex w={{ base: "full", md: "50%" }} gap={3} flexDir="column">
+					<Heading as="h5" size="md">
+						Comments
+					</Heading>
+					{isLoading ? (
+						<Text size="xl">Loading...</Text>
+					) : commentsData?.length ? (
+						commentsData.map((item, index) => (
+							<Flex flexDir="column" key={item.id}>
+								<Text fontSize="md">
+									{index + 1}. {item.body}
+								</Text>
+								<Text
+									fontSize="sm"
+									fontWeight="semibold"
+									color="gray.500"
+									textAlign="right"
+								>
+									created by {item.name}
+								</Text>
+							</Flex>
+						))
+					) : (
+						<Text fontSize="xl">No comments in this post!</Text>
+					)}
+				</Flex>
+			</Flex>
+		</Flex>
+	);
+};
+
+export default BlogDetails;
